6757014515b3f27124ce7ad92ae8d0fa126d8e99
x0n4d0 <romagnolimauricio@gmail.com>
2021-02-02 17:41:12 -0300

    feat: adiciona classe Modal

---
+++ src/script.js
@@ -0,0 +1,11 @@
+const Modal = {
+  open() {
+    const modal = document.querySelector('.modal-overlay');
+    modal.classList.add('active');
+  },
+
+  close() {
+    const modal = document.querySelector('.modal-overlay');
+    modal.classList.remove('active');
+  }
+};
