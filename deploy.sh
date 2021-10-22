4b8ea04a3f5563e2dc83098d815c6432edd1c3cd
x0n4d0 <romagnolimauricio@gmail.com>
2021-10-22 02:09:44 -0300

    ci: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+chmod +x deploy.sh
+
+mv ./public/* .
+rm README.md LICENSE *.js *.json
+rm -rf ./public
+rm -rf ./scripts
+rm -rf ./.github
+git add -A
+git commit -m "Deploy"
+git push && git checkout master
